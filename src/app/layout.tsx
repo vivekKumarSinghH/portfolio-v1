9e9e4e7d35915e54afac33245f3673a5feafe7c2
Vivek Kumar <72158561+vivekKumarSinghH@users.noreply.github.com>
2024-04-17 11:36:26 +0530

    chore: :green_heart: add analytics and page insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -64,6 +65,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <SpeedInsights />
+      <Analytics />
     </html>
   );
 }
